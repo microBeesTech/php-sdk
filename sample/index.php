1b0e6181b7984826634677e08b3c48c63a162f9c
Marco Lettieri <m.lettieri@microbees.com>
2015-10-27 10:26:14 +0100

    Update index.php

--- sample/index.php
+++ sample/index.php
@@ -4,5 +4,5 @@ ini_set('display_errors', 1);
 require("../microBeesSDK.php");
 $obj = new microBees(array("DEBUG"=>true));
 $obj->getAccessToken("example@microbees.com","Example12345","myClientID","myClientSecret");
-$obj->doRequest("api/refreshTranslations","{'foo','example'}");
+$obj->doRequest("v/1_0/getMyProfile","");
 ?> 
